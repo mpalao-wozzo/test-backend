8a8d972b21e262b14cc4f9da702740fd3bd2e6c3
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-03 15:08:40 +0200

    Artist model have been edited to remove required value from description.

--- src/models/artist.js
+++ src/models/artist.js
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose';
 
 const artistSchema = new Schema({
   name: { type: String, required: true, trim: true, maxlength: 50 },
-  description: { type: String, required: true, trim: true, maxlength: 300 },
+  description: { type: String, trim: true, maxlength: 300 },
   active: { type: Boolean, default: true, require: true },
   deleted: { type: Boolean, default: false, required: true },
 });
