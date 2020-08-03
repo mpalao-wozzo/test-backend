1ba692246fea33587e9881bf9c856827125d25fe
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-03 12:17:59 +0200

    Artist model has been created.

---
+++ src/models/artist.js
@@ -0,0 +1,11 @@
+import { Schema, model } from 'mongoose';
+
+const artistSchema = new Schema({
+  name: { type: String, required: true, trim: true, maxlength: 50 },
+  description: { type: String, required: true, trim: true, maxlength: 300 },
+  active: { type: Boolean, default: true, require: true },
+  deleted: { type: Boolean, default: false, required: true },
+});
+
+const artistModel = model('artist', artistSchema);
+export default artistModel;
