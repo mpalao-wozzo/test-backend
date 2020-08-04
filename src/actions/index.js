a2f1652c5bf9823480a1302617431248e5ff0a86
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-04 12:44:16 +0200

    Ordered alphabetically

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
+import artist from './artistActions';
 import error from './errorActions';
 import image from './imageActions';
 import user from './userActions';
 import userRole from './userRoleActions';
 import refreshToken from './refreshTokenActions';
-import artist from './artistActions';
 
+export const artistActions = artist;
 export const errorActions = error;
 export const imageActions = image;
 export const userActions = user;
 export const userRoleActions = userRole;
 export const refreshTokenActions = refreshToken;
-export const artistActions = artist;
