72263b61307188c9b197bd02cd61ef34dfe7a767
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-06 16:51:14 +0200

    Song model created

---
+++ src/models/song.js
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose';
+
+const songSchema = new Schema({
+  name: { type: String, required: true, trim: true, unique: true, maxlength: 75 },
+  imgUrl: { type: String, maxlength: 1000 },
+  songUrl: { type: String, maxlength: 1000 },
+  artistId: { type: Schema.Types.ObjectId, ref: 'Artist', required: true },
+  genreId: { type: Schema.Types.ObjectId, ref: 'Genre', required: true },
+  release_date: { type: Date },
+  album: { type: String, trim: true, maxlength: 75 },
+  active: { type: Boolean, default: true, require: true },
+  deleted: { type: Boolean, default: false, required: true },
+});
+
+const songModel = model('song', songSchema);
+export default songModel;
