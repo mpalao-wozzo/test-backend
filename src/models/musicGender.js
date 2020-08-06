cd87f747806f27d07d1e78a42552ffff3178b58d
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-06 10:42:23 +0200

    name now is unique

--- src/models/musicGender.js
+++ src/models/musicGender.js
@@ -1,7 +1,7 @@
 import { Schema, model } from 'mongoose';
 
 const musicGenderSchema = new Schema({
-  name: { type: String, required: true, trim: true, maxlength: 50 },
+  name: { type: String, required: true, trim: true, unique: true, maxlength: 50 },
   active: { type: Boolean, default: true, require: true },
   deleted: { type: Boolean, default: false, required: true },
 });
