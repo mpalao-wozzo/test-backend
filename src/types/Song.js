129bf8bae78954271be27588efe6b35d9d670fc5
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-07 07:54:05 +0200

    Song type have been added

---
+++ src/types/Song.js
@@ -0,0 +1,31 @@
+import { GraphQLID, GraphQLNonNull, GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLInputObjectType } from 'graphql';
+
+export const SongInput = new GraphQLInputObjectType({
+  name: 'SongInput',
+  fields: () => ({
+    _id: { type: GraphQLID },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    imgUrl: { type: GraphQLString },
+    songUrl: { type: GraphQLString },
+    artistId: { type: new GraphQLNonNull(GraphQLID) },
+    genreId: { type: new GraphQLNonNull(GraphQLID) },
+    releaseDate: { type: GraphQLString },
+    active: { type: GraphQLBoolean },
+    deleted: { type: GraphQLBoolean },
+  }),
+});
+
+export default new GraphQLObjectType({
+  name: 'Song',
+  fields: () => ({
+    _id: { type: new GraphQLNonNull(GraphQLID) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    imgUrl: { type: new GraphQLNonNull(GraphQLString) },
+    songUrl: { type: new GraphQLNonNull(GraphQLString) },
+    artistId: { type: new GraphQLNonNull(GraphQLID) },
+    genreId: { type: new GraphQLNonNull(GraphQLID) },
+    releaseDate: { type: new GraphQLNonNull(GraphQLString) },
+    active: { type: new GraphQLNonNull(GraphQLBoolean) },
+    deleted: { type: new GraphQLNonNull(GraphQLBoolean) },
+  }),
+});
