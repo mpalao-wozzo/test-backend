b2bc04ed178673e42cf1e3bf5d34733b9fc6bf08
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-11 16:36:25 +0200

    Lengths moved to constants.

--- src/models/song.js
+++ src/models/song.js
@@ -1,13 +1,14 @@
 import { Schema, model } from 'mongoose';
+import { MAX_LENGTH_NAME, MAX_LENGTH_DESCRIPTION } from '../utils/constants';
 
 const songSchema = new Schema({
-  name: { type: String, required: true, trim: true, unique: true, maxlength: 75 },
-  imgUrl: { type: String, maxlength: 1000 },
-  songUrl: { type: String, maxlength: 1000 },
+  name: { type: String, required: true, trim: true, unique: true, maxlength: MAX_LENGTH_NAME },
+  imgUrl: { type: String, maxlength: MAX_LENGTH_DESCRIPTION },
+  songUrl: { type: String, maxlength: MAX_LENGTH_DESCRIPTION },
   artistId: { type: Schema.Types.ObjectId, ref: 'Artist', required: true },
   genreId: { type: Schema.Types.ObjectId, ref: 'Genre', required: true },
   releaseDate: { type: String },
-  album: { type: String, trim: true, maxlength: 75 },
+  album: { type: String, trim: true, maxlength: MAX_LENGTH_NAME },
   active: { type: Boolean, default: true, require: true },
   deleted: { type: Boolean, default: false, required: true },
 });
